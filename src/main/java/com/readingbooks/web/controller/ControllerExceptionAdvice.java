17b804ee8563001901d880293fd2c5bb6ee063f5
jaykayBaek <jaykaybaek@gmail.com>
2023-06-30 13:30:43 +0900

    추가 : 예외 어드바이스 추가

---
+++ src/main/java/com/readingbooks/web/controller/ControllerExceptionAdvice.java
@@ -0,0 +1,44 @@
+package com.readingbooks.web.controller;
+
+import com.readingbooks.web.exception.base.NotFoundException;
+import com.readingbooks.web.exception.base.PresentException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ControllerExceptionAdvice {
+
+    @ExceptionHandler(PresentException.class)
+    public ResponseEntity<Object> handlePresentException(Exception e){
+        log.info("예외 발생 - PresentException : ", e.getMessage());
+
+        BaseResponse response = new BaseResponse(HttpStatus.CONFLICT, e.getMessage(), false);
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(response);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Object> handlerNotFoundException(Exception e){
+        log.info("예외 발생 - NotFoundException : ", e.getMessage());
+
+        BaseResponse response = new BaseResponse(HttpStatus.NOT_FOUND, e.getMessage(), false);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(response);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handlerIllegalArgumentException(Exception e){
+        log.info("예외 발생 - IllegalArgumentException : ", e.getMessage());
+
+        BaseResponse response = new BaseResponse(HttpStatus.BAD_REQUEST, e.getMessage(), false);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(response);
+    }
+}
