47f94b749b62cd0396fb4dc0a61dc768e3126738
jaykayBaek <jaykaybaek@gmail.com>
2023-07-04 23:59:06 +0900

    수정 : @Getter를 추가하지 않아 HttpMediaTypeNotAcceptableException 예외 해결

--- src/main/java/com/readingbooks/web/controller/BaseResponse.java
+++ src/main/java/com/readingbooks/web/controller/BaseResponse.java
@@ -1,9 +1,11 @@
 package com.readingbooks.web.controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
+@Getter
 public class BaseResponse {
     private final HttpStatus status;
     private final String message;
