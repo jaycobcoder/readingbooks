9a26131e7e137e1b3e372e343fc3102d1a734c96
jaykayBaek <jaykaybaek@gmail.com>
2023-07-10 16:23:44 +0900

    수정 : BookContent 엔티티 Many to one 관계를 one to one으로 변경

--- src/main/java/com/readingbooks/web/domain/entity/book/BookContent.java
+++ src/main/java/com/readingbooks/web/domain/entity/book/BookContent.java
@@ -2,11 +2,13 @@ package com.readingbooks.web.domain.entity.book;
 
 import com.readingbooks.web.domain.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class BookContent extends BaseEntity {
     @Id
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "book_id")
     private Book book;
     private String content;
