b29d44164126680a39d2838ce2f1411265f1a04d
jaykayBaek <jaykaybaek@gmail.com>
2023-07-12 19:20:48 +0900

    추가 : 이미지 리소스 핸들러
    특정 url 접근 시 이미지 접근 가능하도록 리소스 핸들러 등록하였습니다.

---
+++ src/main/java/com/readingbooks/config/PathConst.java
@@ -0,0 +1,10 @@
+package com.readingbooks.config;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PathConst {
+    public static final String IMAGE_PATH = "/images/book/**";
+    public static final String IMAGE_SAVE_PATH = "D:/file/";
+}
---
+++ src/main/java/com/readingbooks/config/ResourceConfig.java
@@ -0,0 +1,16 @@
+package com.readingbooks.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceConfig implements WebMvcConfigurer {
+
+    private String imageResourcePath = "file:///"+ PathConst.IMAGE_SAVE_PATH;
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(PathConst.IMAGE_PATH)
+                .addResourceLocations(imageResourcePath);
+    }
+}
