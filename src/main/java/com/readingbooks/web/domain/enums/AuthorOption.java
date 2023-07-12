24c80309697124f36cdeb1af26b5690b18e36d78
jaykayBaek <jaykaybaek@gmail.com>
2023-07-12 15:50:04 +0900

    수정 : 작가 옵션 enum 설명 추가

--- src/main/java/com/readingbooks/web/domain/enums/AuthorOption.java
+++ src/main/java/com/readingbooks/web/domain/enums/AuthorOption.java
@@ -1,5 +1,13 @@
 package com.readingbooks.web.domain.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum AuthorOption {
-    AUTHOR, TRANSLATOR, ILLUSTRATOR
+    AUTHOR("작가"), TRANSLATOR("번역가"), ILLUSTRATOR("삽화가");
+
+    private final String korean;
+
 }
