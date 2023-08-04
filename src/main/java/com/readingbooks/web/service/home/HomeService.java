62f899f4dd0cbe0205785abc58a548bb15f0bb34
jaykayBaek <jaykaybaek@gmail.com>
2023-08-04 15:19:19 +0900

    픽스 : HomeService 읽기 전용 트랜잭션 추가

--- src/main/java/com/readingbooks/web/service/home/HomeService.java
+++ src/main/java/com/readingbooks/web/service/home/HomeService.java
@@ -4,15 +4,16 @@ import com.readingbooks.web.repository.book.BookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Slf4j
 public class HomeService {
     private final BookRepository bookRepository;
-
 
     public List<HomeBooksResponse> findBestBooks() {
         return bookRepository.findBestBooks();
