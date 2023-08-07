a77907a6b0d70e656917a29cf379b95586b839db
jaykayBaek <jaykaybaek@gmail.com>
2023-08-07 21:36:19 +0900

    리펙토링 : 가독성을 위해 주석 및 코드 리펙토링

--- src/main/java/com/readingbooks/web/controller/book/BookController.java
+++ src/main/java/com/readingbooks/web/controller/book/BookController.java
@@ -41,47 +41,56 @@ public class BookController {
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public String book(@PathVariable String isbn, HttpServletResponse response, Principal principal, Model model) throws IOException {
         /* --- 도서 정보 --- */
-        BookInformationResponse bookInformation = bookInformationService.findBookInformation(isbn);
-
-        /* --- isbn에 해당하는 도서가 없다면 404 --- */
-        if(bookInformation == null){
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        BookInformationResponse bookInformation = findBookInformation(isbn, response);
+        if (bookInformation == null) {
             return "error/404";
         }
+        model.addAttribute("information", bookInformation);
 
-        /* --- 시리즈 정보 --- */
+        /* --- 도서 그룹에 포함되어있는 도서들 --- */
         List<BookGroupInformationResponse> seriesInformation = bookInformationService.findSeriesInformation(isbn);
+        model.addAttribute("booksInGroup", seriesInformation);
 
         /* --- 작가의 이름과 아이디 --- */
         List<AuthorNameAndIdResponse> authorNameAndIdList = bookInformationService.findAuthorNameAndIdList(isbn);
+        model.addAttribute("authorNameAndIdList", authorNameAndIdList);
 
         /* --- 작가, 번역가, 삽화가의 국적, 소개와 같은 정보들 --- */
-        AuthorInformationResponse authorInformation = null;
-        Long authorId = bookInformation.getAuthorDto().getAuthorId();
-
-        /* --- 만약 작가 아이디를 조작한 뒤 요청했다면, ControllerAdvice로 예외 처리를 하지 않기 위해 null로 반환 --- */
-        try{
-            authorInformation = bookInformationService.findAuthorInformation(isbn, authorId);
-        } catch (AuthorNotfoundException e){
-            authorInformation = null;
-        }
+        AuthorInformationResponse authorInformation = findAuthorInformation(isbn, bookInformation);
+        model.addAttribute("authorInformation", authorInformation);
 
         /* --- 내가 작성한 리뷰 --- */
-        MyWroteReviewInBookResponse myReview = null;
-        try{
-            Member member = memberService.findMember(principal);
-            Long memberId = member.getId();
+        MyWroteReviewInBookResponse myReview = findMyWroteReviewWhenMemberLoggedIn(isbn, principal, model);
+        model.addAttribute("myReview", myReview);
 
-            myReview = reviewService.findWroteReview(memberId, isbn);
-            model.addAttribute("isLogin", true);
-        } catch (NotLoginException e){
-            model.addAttribute("isLogin", false);
-        }
-
-        /* --- 도서에 작성된 리뷰 --- */
+        /* --- 도서에 작성된 리뷰, 리뷰의 평점과 몇 명 평가했는지 정보 --- */
         List<ReviewResponse> reviews = reviewService.findReviews(isbn);
+        model.addAttribute("reviews", reviews);
+        addAttributeTotalReviewRatingAndReviewCount(model, reviews);
+
+        /* --- 본인 확인용 이메일 --- */
+        String email = findEmailForCheckingIdentification(principal);
+        model.addAttribute("email", email);
+
+        return "book/book";
+    }
+
+    @GetMapping("/book/{isbn}/{authorId}")
+    @ResponseBody
+    public ResponseEntity<Object> getAuthorInformation(@PathVariable String isbn, @PathVariable Long authorId){
+        AuthorInformationResponse authorInformation = bookInformationService.findAuthorInformation(isbn, authorId);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(authorInformation);
+    }
+
+    /**
+     * 리뷰를 몇 명이 평가했고, 평점의 평균 구하는 메소드
+     * @param model
+     * @param reviews
+     */
+    private void addAttributeTotalReviewRatingAndReviewCount(Model model, List<ReviewResponse> reviews) {
         if(reviews.size() != 0){
-            /* --- 몇 명이 평가했고, 평점의 평균 구하기 --- */
             int totalReviewRating = 0;
             int reviewCount = 0;
             for (ReviewResponse review : reviews) {
@@ -95,31 +104,79 @@ public class BookController {
             model.addAttribute("starRatingAvg", 0.0);
             model.addAttribute("reviewCount", 0);
         }
+    }
 
-        /* --- 본인 확인용 이메일 --- */
+    /**
+     * 댓글 삭제 버튼 등은 로그인한 정보 Principal에서 email과 댓글을 작성한 email이 일치할 때 버튼을 렌더링 하기에 email 정보를 응답하는 메소드
+     * @param principal
+     * @return email
+     */
+    private String findEmailForCheckingIdentification(Principal principal) {
         String email = "";
         if(principal != null){
             email = principal.getName();
         }else{
             email = "";
         }
-        model.addAttribute("email", email);
-
-        model.addAttribute("myReview", myReview);
-        model.addAttribute("reviews", reviews);
-        model.addAttribute("information", bookInformation);
-        model.addAttribute("booksInGroup", seriesInformation);
-        model.addAttribute("authorNameAndIdList", authorNameAndIdList);
-        model.addAttribute("authorInformation", authorInformation);
-        return "book/book";
+        return email;
     }
 
-    @GetMapping("/book/{isbn}/{authorId}")
-    @ResponseBody
-    public ResponseEntity<Object> getAuthorInformation(@PathVariable String isbn, @PathVariable Long authorId){
-        AuthorInformationResponse authorInformation = bookInformationService.findAuthorInformation(isbn, authorId);
+    /**
+     * 만약 로그인했다면 내가 작성한 리뷰를 응답
+     * @param isbn
+     * @param principal
+     * @param model
+     * @return
+     */
+    private MyWroteReviewInBookResponse findMyWroteReviewWhenMemberLoggedIn(String isbn, Principal principal, Model model) {
+        MyWroteReviewInBookResponse myReview = null;
+        try{
+            Member member = memberService.findMember(principal);
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(authorInformation);
+            Long memberId = member.getId();
+
+            myReview = reviewService.findWroteReview(memberId, isbn);
+            model.addAttribute("isLogin", true);
+        } catch (NotLoginException e){
+            model.addAttribute("isLogin", false);
+        }
+        return myReview;
+    }
+
+    /**
+     * 작가의 정보를 응답하는 메소드
+     * @param isbn
+     * @param bookInformation
+     * @return AuthorInformation Dto
+     */
+    private AuthorInformationResponse findAuthorInformation(String isbn, BookInformationResponse bookInformation) {
+        AuthorInformationResponse authorInformation = null;
+        Long authorId = bookInformation.getAuthorDto().getAuthorId();
+
+        /* --- 만약 작가 아이디를 조작한 뒤 요청했다면, ControllerAdvice로 예외 처리를 하지 않기 위해 null로 반환 --- */
+        try{
+            authorInformation = bookInformationService.findAuthorInformation(isbn, authorId);
+        } catch (AuthorNotfoundException e){
+            authorInformation = null;
+        }
+        return authorInformation;
+    }
+
+    /**
+     * 도서의 정보를 응답하는 메소드
+     * @param isbn
+     * @param response
+     * @return BookInformation Dto
+     * @throws IOException
+     */
+    private BookInformationResponse findBookInformation(String isbn, HttpServletResponse response) throws IOException {
+        BookInformationResponse bookInformation = bookInformationService.findBookInformation(isbn);
+
+        /* --- isbn에 해당하는 도서가 없다면 404 --- */
+        if(bookInformation == null){
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return null;
+        }
+        return bookInformation;
     }
 }
